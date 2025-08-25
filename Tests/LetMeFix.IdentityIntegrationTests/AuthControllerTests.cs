7b43da617bf2d416081b38af7252faad0f8de65e
FatihAydinn <a.fatihaydn@gmail.com>
2025-08-25 23:57:37 +0300

    integration test: register

---
+++ Tests/LetMeFix.IdentityIntegrationTests/AuthControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using LetMeFix.Domain.Entities;
+using LetMeFix.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetMeFix.IdentityIntegrationTests
+{
+    public class AuthControllerTests : IClassFixture<AuthWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthControllerTests(AuthWebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Register()
+        {
+            var request = new
+            {
+                Email = "newuser@gmail.com",
+                UserName = "newuser",
+                Name = "New",
+                Lastname = "User",
+                Password = "1234Xyz!"
+            };
+
+            var response = await _httpClient.PostAsJsonAsync("/api/identity/register", request);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ Tests/LetMeFix.IdentityIntegrationTests/AuthWebApplicationFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LetMeFix.Domain.Entities;
+using LetMeFix.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace LetMeFix.IdentityIntegrationTests
+{
+    public class AuthWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<UserDbContext>));
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                services.AddDbContext<UserDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDb");
+                });
+
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+                db.Database.EnsureCreated();
+            });
+        }
+    }
+}
