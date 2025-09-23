f1a6922479b34a2c892a3b84e23f7ea7c18c1161
FatihAydinn <a.fatihaydn@gmail.com>
2025-09-23 21:14:47 +0300

    UserInformationService completed

---
+++ Infrastructure/LetMeFix.Persistence/Services/UserInformationService.cs
@@ -0,0 +1,48 @@
+﻿using LetMeFix.Domain.Entities;
+using LetMeFix.Domain.Interfaces;
+using Microsoft.Identity.Client;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetMeFix.Persistence.Services
+{
+    public class UserInformationService : IGenericRepository<UserInformations>
+    {
+        private readonly IMongoCollection<UserInformations> _collection;
+
+        public UserInformationService(IMongoDatabase database)
+        {
+            _collection = database.GetCollection<UserInformations>("UserInformations");
+        }
+
+        public async Task AddAsync(UserInformations entity)
+        {
+            await _collection.InsertOneAsync(entity);
+        }
+
+        public async Task<UserInformations> GetByIdAsync(string id)
+        {
+            return await _collection.Find(x => x.UserId == id).FirstOrDefaultAsync();
+        }
+        
+        public async Task UpdateAsync(UserInformations entity)
+        {
+            await _collection.ReplaceOneAsync(x => x.UserId == entity.UserId, entity);
+        }
+        
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserInformations>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
