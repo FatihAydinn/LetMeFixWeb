555f9a77c8bc2b6ed159f1b7e0ea9a33839685b2
FatihAydinn <a.fatihaydn@gmail.com>
2025-11-26 22:28:00 +0300

    EmailService added

---
+++ Infrastructure/LetMeFix.Infrastructure/LetMeFix.Infrastructure/Services/EmailService.cs
@@ -0,0 +1,17 @@
+﻿using LetMeFix.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetMeFix.Infrastructure.Services
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendEmailAsync(string to, string subject, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
