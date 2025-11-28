3ee299445f4d6222fcf9c35166c14d1347f767d3
FatihAydinn <a.fatihaydn@gmail.com>
2025-11-28 23:51:19 +0300

    smtp added

--- Infrastructure/LetMeFix.Infrastructure/LetMeFix.Infrastructure/Services/EmailService.cs
+++ Infrastructure/LetMeFix.Infrastructure/LetMeFix.Infrastructure/Services/EmailService.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +13,16 @@ namespace LetMeFix.Infrastructure.Services
     {
         public Task SendEmailAsync(string to, string subject, string body)
         {
-            throw new NotImplementedException();
+            var email = "";
+            var pass = "";
+
+            var client = new SmtpClient("smtp-mail.outlook.com", 587)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(email, pass)
+            };
+
+            return client.SendMailAsync(new MailMessage(from: email, to: to, subject, body));
         }
     }
 }
