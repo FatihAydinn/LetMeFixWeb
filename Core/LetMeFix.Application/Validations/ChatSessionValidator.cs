557d1912bfbf6d00e7f774a396b8266d77e39ee0
FatihAydinn <a.fatihaydn@gmail.com>
2026-01-11 23:21:40 +0300

    userinformation and chatsession validation added

---
+++ Core/LetMeFix.Application/Validations/ChatSessionValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using LetMeFix.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetMeFix.Application.Validations
+{
+    public class ChatSessionValidator : AbstractValidator<ChatSessionDTO>
+    {
+        public ChatSessionValidator()
+        {
+            RuleFor(x => x.MessageContent[0].Content).NotEmpty().MinimumLength(2).MaximumLength(50).When(x => x.MessageContent[0].Type == "text").WithMessage("Message must be between 2 and 200 characters!");
+        }
+    }
+}
---
+++ Core/LetMeFix.Application/Validations/UserInformationValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using LetMeFix.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetMeFix.Application.Validations
+{
+    public class UserInformationValidation : AbstractValidator<UserInformations>
+    {
+        public UserInformationValidation() {
+            RuleFor(x => x.BirthDate).NotEmpty().Must(age => age <= DateTime.Today.AddYears(-18)).WithMessage("The user must be over 18!");
+            RuleFor(x => x.MobileNumber).Length(10).WithMessage("Please enter a 10-digit phone number (e.g., 5551234567)").Must(phone => phone.All(char.IsDigit)).WithMessage("Enter only numbers!");
+            RuleFor(x => x.Skills).NotEmpty().Must(y => y.Count >= 3).WithMessage("You must select at least 3 skills!");
+        }
+    }
+}
